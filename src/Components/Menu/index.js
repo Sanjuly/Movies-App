602a1e6873762ef6aace803e030fec17695eff3b
Sandra Cordoba <sandracordoba16@gmail.com>
2023-10-21 20:19:37 -0300

    Creating Menu Components with the Navlink Tool

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -1,11 +1,46 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Menu() {
     return (
-        <div>
-            <p>Menu</p>
-        </div>
+        <nav>
+            <ul>
+                {routes.map(route => (
+                    <li> 
+                        <NavLink
+                            key={route.to}
+                            to={route.to}
+                            style={({isActive}) => ({
+                            color: isActive ? 'red' : 'blue',})}>
+                            {route.text}
+                        </NavLink>
+                    </li>
+                ))}
+            </ul>
+            
+        </nav>
     )
 }
+const routes = []
+routes.push({
+    to: '/',
+    text: 'Home'
+})
+routes.push({
+    to: '/category',
+    text: 'Category'
+})
+routes.push({
+    to: '/details',
+    text: 'Movie Details'
+})
+routes.push({
+    to: '/search',
+    text: 'Search'
+})
+routes.push({
+    to: '/trends',
+    text: 'Trends'
+})
 
 export { Menu }
